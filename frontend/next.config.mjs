89cb8684bbd79bd1ac405253916be61cd3fd0b14
Quah Kian Seong <e0727337@u.nus.edu>
2024-08-08 15:08:56 +0800

    Fix dependencies

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  compiler: {
-    styledComponents: true,
+  experimental: {
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
 };
 
